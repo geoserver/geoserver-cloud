60251b50195d5f2e09975eef50dd0a3035fa721e
Gabriel Roldan <gabriel.roldan@gmail.com>
2020-07-10 12:07:36 -0300

    config: Initial set up of config service
    Config service using discovery-first initialization through
    /bootstrap.yml
    
    See https://cloud.spring.io/spring-cloud-static/spring-cloud.html#_spring_cloud_config_server

---
+++ support-services/config/src/main/java/org/geoserver/cloud/config/ConfigApplication.java
@@ -0,0 +1,14 @@
+package org.geoserver.cloud.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigApplication.class).run(args);
+    }
+}
