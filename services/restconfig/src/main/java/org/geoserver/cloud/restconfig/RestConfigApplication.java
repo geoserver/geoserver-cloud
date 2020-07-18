8184ab2dea09f9128f1ce3694553306129bf3391
Gabriel Roldan <gabriel.roldan@gmail.com>
2020-07-17 23:56:39 -0300

    Initial implementation of restconfig service
    Requests to <gateway-service>/rest/** are correctly dispatched to
    <restconfig-v1-service>/rest/**, yet geoserver's rest FTL templates for
    HTML output create links like to /rest/null/**, REVISIT.
    
    And of course these URL's as any other one generated by geoserver (e.g.
    in capabilities documents) still need to be proxified, since they point
    to the container's internal IP and not the gateway's.

---
+++ services/restconfig/src/main/java/org/geoserver/cloud/restconfig/RestConfigApplication.java
@@ -0,0 +1,51 @@
+/* (c) 2020 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.cloud.restconfig;
+
+import org.geoserver.cloud.core.GeoServerServletConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+@SpringBootApplication(
+    exclude = { //
+        DataSourceAutoConfiguration.class, //
+        DataSourceTransactionManagerAutoConfiguration.class, //
+        HibernateJpaAutoConfiguration.class, //
+        SecurityAutoConfiguration.class, //
+        UserDetailsServiceAutoConfiguration.class, //
+        ManagementWebSecurityAutoConfiguration.class
+    }
+)
+@Import({ //
+    GeoServerServletConfig.class,
+    org.geoserver.rest.RestConfiguration.class //
+})
+public class RestConfigApplication {
+
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(RestConfigApplication.class, args);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    @ConditionalOnMissingBean(RequestMappingHandlerMapping.class)
+    public @Bean RequestMappingHandlerMapping requestMappingHandlerMapping() {
+        RequestMappingHandlerMapping mapping = new RequestMappingHandlerMapping();
+        return mapping;
+    }
+}
