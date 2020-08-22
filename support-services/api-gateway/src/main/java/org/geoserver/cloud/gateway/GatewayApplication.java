f365d1135c68216a97325b8b68e73fff78f54eec
Gabriel Roldan <gabriel.roldan@gmail.com>
2020-08-21 22:57:38 -0300

    Match OWS service query parameters in case-insensitively
    Add a gateway predicate factory to match query parameters
    using regular expressions for both the query parameter name and value,
    and configure the gateway OWS routes to match the service=<ows name>
    query parameter in a case insensitive fashion using regular expressions.

--- support-services/api-gateway/src/main/java/org/geoserver/cloud/gateway/GatewayApplication.java
+++ support-services/api-gateway/src/main/java/org/geoserver/cloud/gateway/GatewayApplication.java
@@ -1,18 +1,17 @@
-/* (c) 2020 Open Source Geospatial Foundation - all rights reserved
- * This code is licensed under the GPL 2.0 license, available at the root
- * application directory.
+/*
+ * (c) 2020 Open Source Geospatial Foundation - all rights reserved This code is licensed under the
+ * GPL 2.0 license, available at the root application directory.
  */
 package org.geoserver.cloud.gateway;
 
+import org.geoserver.cloud.gateway.predicate.RegExpQueryRoutePredicateFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
 public class GatewayApplication {
 
     public static void main(String[] args) {
@@ -21,5 +20,13 @@ public class GatewayApplication {
 
     public @Bean RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes().build();
+    }
+
+    /**
+     * Custom gateway predicate factory to support matching by regular expressions on both name and
+     * value of query parameters
+     */
+    public @Bean RegExpQueryRoutePredicateFactory regExpQueryRoutePredicateFactory() {
+        return new RegExpQueryRoutePredicateFactory();
     }
 }
