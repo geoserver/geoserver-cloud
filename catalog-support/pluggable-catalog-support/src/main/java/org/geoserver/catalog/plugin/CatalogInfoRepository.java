1ad636a5915aec764f800d5d6c0b37e21ae62813
Gabriel Roldan <gabriel.roldan@gmail.com>
2020-09-26 07:21:02 -0300

    some CatalogInfoRepository javadocs

--- catalog-support/pluggable-catalog-support/src/main/java/org/geoserver/catalog/plugin/CatalogInfoRepository.java
+++ catalog-support/pluggable-catalog-support/src/main/java/org/geoserver/catalog/plugin/CatalogInfoRepository.java
@@ -19,14 +19,63 @@ import org.geoserver.catalog.StyleInfo;
 import org.geoserver.catalog.WorkspaceInfo;
 import org.springframework.lang.Nullable;
 
+/**
+ * Raw data access API for {@link CatalogInfo} back-end implementations.
+ *
+ * <p>This is a null-free, DDD inspired "repository" API to provide storage and querying of plain
+ * {@link CatalogInfo} objects, with precise semantics. No method that receives an argument can
+ * receive {@code null}. In cases where different semantics shall be applied, each specialization
+ * provides alternative, semantically clear query methods. For example, {@link
+ * StyleRepository#findAllByNullWorkspace()} and {@link
+ * StyleRepository#findAllByWorkspace(WorkspaceInfo)} are self-explanatory, no magic is involved nor
+ * decision making, which are handled at a higer level of abstraction ({@code CatalogFacade} and/or
+ * {@code Catalog}).
+ *
+ * <p>All query methods that could return zero or one result, return {@link Optional}. All query
+ * methods that could return zero or more results, return {@link Stream}.
+ *
+ * <p>Care shall be taken that {@code Stream} implements {@link AutoCloseable} and hence it is
+ * expected for users of this api to properly close the received stream, may the implementation be
+ * using live connections to some back-end storage and need to release resources.
+ *
+ * @see WorkspaceRepository
+ * @see NamespaceRepository
+ * @see StoreRepository
+ * @see ResourceRepository
+ * @see LayerRepository
+ * @see LayerGroupRepository
+ * @see StyleRepository
+ * @see MapRepository
+ * @param <T>
+ */
 public interface CatalogInfoRepository<T extends CatalogInfo> {
 
+    /**
+     * @return the less concrete {@link CatalogInfo} type this repository handles (e.g. if {@link
+     *     ResourceInfo}, then this repository handles all {@code ResourceInfo} subtypes)
+     */
     Class<T> getContentType();
 
     void add(@NonNull T value);
 
     void remove(@NonNull T value);
 
+    /**
+     * Applies the provided {@link Patch patch} to this repository's copy of the provided {@code
+     * value} object and returns the "patched" object.
+     *
+     * <p>In contrast to a typical {@code save(I value)} method, the patch helps implementors to
+     * apply MVCC (Multi-Version Concurrency Control) semantics to the operation. It provides the
+     * property names and values that need to be changed, and hence the implementation can choose
+     * the best mechanism to apply those specific changes without overriding the whole object. For
+     * example, it can be translated to an SQL {@code UPDATE} with only the required column changes,
+     * or improve concurrency on an in-memory store by only having to lock the live-object while the
+     * patch is applied.
+     *
+     * <p>The provided value shall not be modified by this method. Whether the provided value and
+     * the updated repository copy refer to the same object instance is up to the implementation,
+     * but they're considered different from the point of view of the API.
+     */
     <I extends T> I update(@NonNull I value, @NonNull Patch patch);
 
     void dispose();
@@ -35,6 +84,12 @@ public interface CatalogInfoRepository<T extends CatalogInfo> {
         return findAll(Query.all(getContentType()));
     }
 
+    /**
+     * Returns all objects in this repository that satisfy the query criteria (type and filter),
+     * additional restrictions such as paging and order.
+     *
+     * @see Query
+     */
     <U extends T> Stream<U> findAll(Query<U> query);
 
     /** Looks up a CatalogInfo by class and identifier */
--- catalog-support/pluggable-catalog-support/src/main/java/org/geoserver/config/plugin/ConfigRepository.java
+++ catalog-support/pluggable-catalog-support/src/main/java/org/geoserver/config/plugin/ConfigRepository.java
@@ -6,22 +6,41 @@ package org.geoserver.config.plugin;
 
 import java.util.Optional;
 import java.util.stream.Stream;
+import org.geoserver.catalog.Info;
 import org.geoserver.catalog.WorkspaceInfo;
 import org.geoserver.config.GeoServerInfo;
 import org.geoserver.config.LoggingInfo;
 import org.geoserver.config.ServiceInfo;
 import org.geoserver.config.SettingsInfo;
 
+/**
+ * Raw data access API for GeoServer global configuration and per-workspace settings and services
+ * configuration.
+ *
+ * <p>This is a null-free, DDD inspired "repository" API to provide storage and querying of plain
+ * {@link Info} objects, with precise semantics. No method that receives an argument can receive
+ * {@code null}. In cases where different semantics are required, semantically clear query methods
+ * are provided. For example, {@link #getGlobalServices()} and {@link
+ * #getServicesByWorkspace(WorkspaceInfo)} are self-explanatory.
+ *
+ * <p>All query methods that could return zero or one result, return {@link Optional}. All query
+ * methods that could return zero or more results, return {@link Stream}.
+ *
+ * <p>Care shall be taken that {@code Stream} implements {@link AutoCloseable} and hence it is
+ * expected for users of this api to properly close the received stream, may the implementation be
+ * using live connections to some back-end storage and need to release resources.
+ */
 public interface ConfigRepository {
 
     /** The global geoserver configuration. */
     Optional<GeoServerInfo> getGlobal();
 
-    /** Sets the global configuration. */
+    /** Sets the global configuration, replacing the current one completely. */
     void setGlobal(GeoServerInfo global);
 
     /**
-     * The settings configuration for the specified workspace, or <code>null</code> if non exists.
+     * The settings configuration for the specified workspace, or {@code Optional.empty()} if non
+     * exists.
      */
     Optional<SettingsInfo> getSettingsByWorkspace(WorkspaceInfo workspace);
 
@@ -37,7 +56,7 @@ public interface ConfigRepository {
     /** The logging configuration. */
     Optional<LoggingInfo> getLogging();
 
-    /** Sets logging configuration. */
+    /** Saves logging configuration, replacing the current one comletely. */
     void setLogging(LoggingInfo logging);
 
     /** Adds a service to the configuration. */
@@ -49,23 +68,23 @@ public interface ConfigRepository {
     /** Saves a service that has been modified. */
     void save(ServiceInfo service);
 
-    /** GeoServer services. */
+    /** GeoServer global services (not attached to any Workspace) */
     Stream<? extends ServiceInfo> getGlobalServices();
 
     /** GeoServer services specific to the specified workspace. */
     Stream<? extends ServiceInfo> getServicesByWorkspace(WorkspaceInfo workspace);
 
     /**
-     * GeoServer global service filtered by class.
+     * GeoServer global service filtered by type.
      *
      * @param clazz The class of the service to return.
      */
     <T extends ServiceInfo> Optional<T> getGlobalService(Class<T> clazz);
 
     /**
-     * GeoServer service specific to the specified workspace and filtered by class.
+     * GeoServer service specific to the specified workspace and filtered by type.
      *
-     * @param workspaceId The workspace the service is specific to.
+     * @param workspace The workspace the service is specific to.
      * @param clazz The class of the service to return.
      */
     <T extends ServiceInfo> Optional<T> getServiceByWorkspace(
@@ -76,33 +95,33 @@ public interface ConfigRepository {
      *
      * @param id The id of the service.
      * @param clazz The type of the service.
-     * @return The service with the specified id, or <code>null</code> if no such service coud be
-     *     found.
+     * @return The service with the specified id, or {@code Optional.empty()} if no such service
+     *     coud be found.
      */
     <T extends ServiceInfo> Optional<T> getServiceById(String id, Class<T> clazz);
 
     /**
-     * Looks up a service by name.
+     * Looks up a service by name and type.
      *
      * @param name The name of the service.
      * @param clazz The type of the service.
-     * @return The service with the specified name or <code>null</code> if no such service could be
-     *     found.
+     * @return The service with the specified name or {@code Optional.empty()} if no such service
+     *     could be found.
      */
     <T extends ServiceInfo> Optional<T> getServiceByName(String name, Class<T> clazz);
 
     /**
-     * Looks up a service by name, specific to the specified workspace.
+     * Looks up a service by name and type, specific to the specified workspace.
      *
      * @param name The name of the service.
      * @param workspaceId The workspace the service is specific to.
      * @param clazz The type of the service.
-     * @return The service with the specified name or <code>null</code> if no such service could be
-     *     found.
+     * @return The service with the specified name or {@code Optional.empty()} if no such service
+     *     could be found.
      */
     <T extends ServiceInfo> Optional<T> getServiceByNameAndWorkspace(
             String name, WorkspaceInfo workspace, Class<T> clazz);
 
-    /** Disposes the configuration, making this repository unusable after */
+    /** Disposes the repository, making it unusable after */
     void dispose();
 }
