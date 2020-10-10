debc9d9105f3f2fe91f838a6459f1760721e288e
Gabriel Roldan <gabriel.roldan@gmail.com>
2020-10-10 16:01:30 -0300

    Include GeoServer jackson modules in event-bus configuration

--- catalog-support/catalog-event-bus/src/main/java/org/geoserver/cloud/autoconfigure/bus/RemoteApplicationEventsAutoConfiguration.java
+++ catalog-support/catalog-event-bus/src/main/java/org/geoserver/cloud/autoconfigure/bus/RemoteApplicationEventsAutoConfiguration.java
@@ -4,11 +4,18 @@
  */
 package org.geoserver.cloud.autoconfigure.bus;
 
+import org.geoserver.catalog.CatalogInfo;
+import org.geoserver.catalog.Info;
 import org.geoserver.cloud.bus.RemoteApplicationEventsConfiguration;
+import org.geoserver.cloud.bus.event.RemoteInfoEvent;
+import org.geoserver.jackson.databind.catalog.GeoServerCatalogModule;
+import org.geoserver.jackson.databind.config.GeoServerConfigModule;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.bus.BusAutoConfiguration;
 import org.springframework.cloud.bus.ConditionalOnBusEnabled;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -20,4 +27,23 @@ import org.springframework.context.annotation.Import;
 @ConditionalOnBusEnabled
 @AutoConfigureAfter(BusAutoConfiguration.class)
 @Import({RemoteApplicationEventsConfiguration.class})
-public class RemoteApplicationEventsAutoConfiguration {}
+public class RemoteApplicationEventsAutoConfiguration {
+
+    /**
+     * Add a {@link GeoServerCatalogModule} to the default jackson spring codecs if not already
+     * present, so {@link CatalogInfo} objects can be used as {@link RemoteInfoEvent} payload
+     */
+    @ConditionalOnMissingBean(GeoServerCatalogModule.class)
+    public @Bean GeoServerCatalogModule geoServerCatalogJacksonModule() {
+        return new GeoServerCatalogModule();
+    }
+
+    /**
+     * Add a {@link GeoServerConfigModule} to the default jackson spring codecs if not already
+     * present, so configuration {@link Info} objects can be used as {@link RemoteInfoEvent} payload
+     */
+    @ConditionalOnMissingBean(GeoServerConfigModule.class)
+    public @Bean GeoServerConfigModule geoServerConfigJacksonModule() {
+        return new GeoServerConfigModule();
+    }
+}
