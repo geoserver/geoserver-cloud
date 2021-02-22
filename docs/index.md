614ab5b19d6db0caea62fc3f534493f81f8ef60a
Gabriel Roldan <gabriel.roldan@gmail.com>
2021-02-22 15:33:19 -0300

    Support and configure default resource limits in docker composition
    Add CPU and memory limits to the service definitions in
    docker-compose.yml.
    
    Need to run docker-compose with the --compatibility flag in order for
    deploy.resources.limits.* to be considered (otherwise they're only
    obeyed in a swarm deploy).
    
    Change JAVA_OPTS to use -XX:MaxRAMPercentage=[0..100]
    -XX:MaxRAM=<size><M|G> and hence set the max heap size dynamically based
    on the available RAM.
    
    The --MaxRAM is set in case docker-compose wasn't (accidentally?) run
    with --compatibility. Otherwise it's unnecessary given the percentage
    set and the memory limit imposed to the container.
    
    Remove command overrides to use dockerize in all services except config
    to wait for discovery, and let the retry configuration be used instead.
    
    Add a log entry when each service is ready to verify the memory and cpu
    limits are seen by the JVM.

--- docs/index.md
+++ docs/index.md
@@ -83,7 +83,7 @@ Download this [docker-compose.yml](deploy/docker-compose/stable/docker-compose.y
 
 ```bash
 $ mkdir gscloud && cd gscloud
-$ wget "https://raw.githubusercontent.com/camptocamp/geoserver-microservices/v0.2.0/docs/deploy/docker-compose/stable/docker-compose.yml" -O docker-compose.yml
+$ wget "https://camptocamp.github.io/geoserver-microservices/deploy/docker-compose/stable/docker-compose.yml"
 ```
 
 Open a terminal and enter the directory where you just downloaded that file, and run `docker-compose pull` to fetch the docker images from [Dockerhub](https://hub.docker.com/u/cloudnativegeoserver/):
@@ -107,8 +107,10 @@ Pulling webui     ... done
 Then start the services with this command:
 
 ```bash
-$ docker-compose up -d
+$ docker-compose --compatibility up -d
 ```
+
+> Note the `--compatibility` argument is required when using a v3 docker-compose file, in order to enable resource the CPU and Memory limits configured in the file for each service.
 
 Wait for the services to start up, check with `docker-compose ps` until they're healthy. May some service had exited, re-run `docker-compose up -d`, initial startup may cause timeouts as services compete for resources on a single machine.
 
--- services/wfs/src/main/java/org/geoserver/cloud/wfs/app/WfsApplication.java
+++ services/wfs/src/main/java/org/geoserver/cloud/wfs/app/WfsApplication.java
@@ -4,9 +4,15 @@
  */
 package org.geoserver.cloud.wfs.app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.util.unit.DataSize;
 
+@Slf4j
 @SpringBootApplication
 public class WfsApplication {
 
@@ -16,5 +22,31 @@ public class WfsApplication {
         } catch (RuntimeException e) {
             System.exit(-1);
         }
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady(ApplicationReadyEvent e) {
+        ConfigurableEnvironment env = e.getApplicationContext().getEnvironment();
+
+        String app = env.getProperty("spring.application.name");
+        String instanceId = env.getProperty("server.instance-id");
+        int cpus = Runtime.getRuntime().availableProcessors();
+        String maxMem;
+        {
+            DataSize maxMemBytes = DataSize.ofBytes(Runtime.getRuntime().maxMemory());
+            double value = maxMemBytes.toKilobytes() / 1024d;
+            String unit = "MB";
+            if (maxMemBytes.toGigabytes() > 0) {
+                value = value / 1024d;
+                unit = "GB";
+            }
+            maxMem = String.format("%.2f %s", value, unit);
+        }
+        log.info(
+                "{} ready. Instance-id: {}, cpus: {}, max memory: {}",
+                app,
+                instanceId,
+                cpus,
+                maxMem);
     }
 }
