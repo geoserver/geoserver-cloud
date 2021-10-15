7c5f2904d87c8edf04d3ac3ea83adee555168ff9
Gabriel Roldan <gabriel.roldan@gmail.com>
2021-10-14 22:34:52 -0300

    Fix data directory backend persiter, legacy needs real CatalogInfo

--- starters/catalog-backend-starter/src/main/java/org/geoserver/cloud/config/datadirectory/DataDirectoryGeoServerLoader.java
+++ starters/catalog-backend-starter/src/main/java/org/geoserver/cloud/config/datadirectory/DataDirectoryGeoServerLoader.java
@@ -5,8 +5,19 @@
 package org.geoserver.cloud.config.datadirectory;
 
 import java.util.List;
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.CatalogInfo;
+import org.geoserver.catalog.Info;
+import org.geoserver.catalog.event.CatalogModifyEvent;
+import org.geoserver.catalog.event.impl.CatalogModifyEventImpl;
+import org.geoserver.catalog.impl.DefaultCatalogFacade;
+import org.geoserver.catalog.impl.ModificationProxy;
+import org.geoserver.catalog.plugin.CatalogPlugin;
+import org.geoserver.config.ConfigurationListener;
 import org.geoserver.config.DefaultGeoServerLoader;
 import org.geoserver.config.GeoServer;
+import org.geoserver.config.GeoServerConfigPersister;
+import org.geoserver.config.GeoServerResourcePersister;
 import org.geoserver.config.ServiceInfo;
 import org.geoserver.config.util.XStreamPersister;
 import org.geoserver.config.util.XStreamServiceLoader;
@@ -24,7 +35,36 @@ public class DataDirectoryGeoServerLoader extends DefaultGeoServerLoader {
     @Override
     protected void loadGeoServer(final GeoServer geoServer, XStreamPersister xp) throws Exception {
         super.loadGeoServer(geoServer, xp);
+
+        GeoServerConfigPersister fixedConfigPersister =
+                new CatalogPluginGeoServerConfigPersister(
+                        geoServer.getCatalog().getResourceLoader(), xp);
+
+        ConfigurationListener configPersister =
+                geoServer
+                        .getListeners()
+                        .stream()
+                        .filter(GeoServerConfigPersister.class::isInstance)
+                        .findFirst()
+                        .orElse(null);
+        if (configPersister != null) {
+            geoServer.removeListener(configPersister);
+        }
+        geoServer.addListener(fixedConfigPersister);
+
         initializeEmptyConfig(geoServer);
+    }
+
+    @Override
+    protected void loadCatalog(Catalog catalog, XStreamPersister xp) throws Exception {
+        super.loadCatalog(catalog, xp);
+
+        catalog.removeListeners(GeoServerConfigPersister.class);
+        catalog.removeListeners(GeoServerResourcePersister.class);
+
+        catalog.addListener(
+                new CatalogPluginGeoServerConfigPersister(catalog.getResourceLoader(), xp));
+        catalog.addListener(new CatalogPluginGeoServerResourcePersister(catalog));
     }
 
     @SuppressWarnings({"unchecked", "rawtypes"})
@@ -50,5 +90,64 @@ public class DataDirectoryGeoServerLoader extends DefaultGeoServerLoader {
         } finally {
             lock.release();
         }
+    }
+
+    /**
+     * A {@link GeoServerConfigPersister} that unwraps the {@link CatalogModifyEvent#getSource()}'s
+     * from a {@link ModificationProxy} before proceeding with {@link
+     * GeoServerConfigPersister#handleModifyEvent super.handleModifyEvent()}, since it works only if
+     * the source is the real {@link Info}, as thrown by the legacy {@link
+     * DefaultCatalogFacade#beforeSaved}, despite it having the following comment: {@code "// TODO:
+     * protect this original object, perhaps with another proxy"}; while {@link CatalogPlugin} fixes
+     * it both by using the modification proxy as the source and by taking full responsibility of
+     * event dispatching instead of mixing it up between catalog and facade.
+     */
+    private static class CatalogPluginGeoServerConfigPersister extends GeoServerConfigPersister {
+        public CatalogPluginGeoServerConfigPersister(
+                GeoServerResourceLoader rl, XStreamPersister xp) {
+            super(rl, xp);
+        }
+
+        @Override
+        public void handleModifyEvent(CatalogModifyEvent event) {
+            CatalogModifyEventImpl e = withRealSource(event);
+            super.handleModifyEvent(e);
+        }
+    }
+
+    /**
+     * A {@link GeoServerResourcePersister} that unwraps the {@link
+     * CatalogModifyEvent#getSource()}'s from a {@link ModificationProxy} before proceeding with
+     * {@link GeoServerResourcePersister#handleModifyEvent super.handleModifyEvent()}, since it
+     * works only if the source is the real {@link Info}, as thrown by the legacy {@link
+     * DefaultCatalogFacade#beforeSaved}, despite it having the following comment: {@code "// TODO:
+     * protect this original object, perhaps with another proxy"}; while {@link CatalogPlugin} fixes
+     * it both by using the modification proxy as the source and by taking full responsibility of
+     * event dispatching instead of mixing it up between catalog and facade.
+     */
+    private static class CatalogPluginGeoServerResourcePersister
+            extends GeoServerResourcePersister {
+
+        public CatalogPluginGeoServerResourcePersister(Catalog catalog) {
+            super(catalog);
+        }
+
+        @Override
+        public void handleModifyEvent(CatalogModifyEvent event) {
+            CatalogModifyEventImpl e = withRealSource(event);
+            super.handleModifyEvent(e);
+        }
+    }
+
+    private static CatalogModifyEventImpl withRealSource(CatalogModifyEvent event) {
+        CatalogInfo source = event.getSource();
+        CatalogInfo real = ModificationProxy.unwrap(source);
+
+        CatalogModifyEventImpl e = new CatalogModifyEventImpl();
+        e.setSource(real);
+        e.setPropertyNames(event.getPropertyNames());
+        e.setOldValues(event.getOldValues());
+        e.setNewValues(event.getNewValues());
+        return e;
     }
 }
