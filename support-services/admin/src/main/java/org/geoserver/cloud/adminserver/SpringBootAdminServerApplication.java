00b9ae7e3bfe4911aea6242cdd7a911926e4c199
Gabriel Roldan <gabriel.roldan@gmail.com>
2021-12-07 20:18:51 -0300

    feature: add gs-cloud-admin-service based on de.codecentric:spring-boot-admin-starter-server

---
+++ support-services/admin/src/main/java/org/geoserver/cloud/adminserver/SpringBootAdminServerApplication.java
@@ -0,0 +1,50 @@
+/*
+ * (c) 2020 Open Source Geospatial Foundation - all rights reserved This code is licensed under the
+ * GPL 2.0 license, available at the root application directory.
+ */
+package org.geoserver.cloud.adminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.util.unit.DataSize;
+
+@Slf4j
+@SpringBootApplication
+@EnableAdminServer
+public class SpringBootAdminServerApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(SpringBootAdminServerApplication.class).run(args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady(ApplicationReadyEvent e) {
+        ConfigurableEnvironment env = e.getApplicationContext().getEnvironment();
+
+        String app = env.getProperty("spring.application.name");
+        String instanceId = env.getProperty("info.instance-id");
+        int cpus = Runtime.getRuntime().availableProcessors();
+        String maxMem;
+        {
+            DataSize maxMemBytes = DataSize.ofBytes(Runtime.getRuntime().maxMemory());
+            double value = maxMemBytes.toKilobytes() / 1024d;
+            String unit = "MB";
+            if (maxMemBytes.toGigabytes() > 0) {
+                value = value / 1024d;
+                unit = "GB";
+            }
+            maxMem = String.format("%.2f %s", value, unit);
+        }
+        log.info(
+                "{} ready. Instance-id: {}, cpus: {}, max memory: {}",
+                app,
+                instanceId,
+                cpus,
+                maxMem);
+    }
+}
