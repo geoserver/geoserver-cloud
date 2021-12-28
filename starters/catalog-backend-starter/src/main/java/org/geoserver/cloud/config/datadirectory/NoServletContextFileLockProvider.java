f34efd5850ce22a5d085b47719346b2d2d07fb5d
Gabriel Roldan <gabriel.roldan@gmail.com>
2021-12-27 21:24:09 -0300

    DataDirectory backend configurer: use a FileLockProvider that explicitly ignored the servlet context

---
+++ starters/catalog-backend-starter/src/main/java/org/geoserver/cloud/config/datadirectory/NoServletContextFileLockProvider.java
@@ -0,0 +1,28 @@
+/*
+ * (c) 2021 Open Source Geospatial Foundation - all rights reserved This code is licensed under the
+ * GPL 2.0 license, available at the root application directory.
+ */
+package org.geoserver.cloud.config.datadirectory;
+
+import java.io.File;
+import javax.servlet.ServletContext;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.geoserver.platform.resource.FileLockProvider;
+
+/**
+ * {@link FileLockProvider} that expects the data directory file at its constructor, overriding
+ * {@link #setServletContext} to be ignored.
+ */
+@Slf4j
+public class NoServletContextFileLockProvider extends FileLockProvider {
+
+    public NoServletContextFileLockProvider(@NonNull File dataDirectory) {
+        super(dataDirectory);
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        log.debug("setServletContext(ServletContext) ignored, data directory explicitly provided.");
+    }
+}
