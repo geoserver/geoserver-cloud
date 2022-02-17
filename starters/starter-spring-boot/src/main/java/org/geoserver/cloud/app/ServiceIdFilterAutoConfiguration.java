88b111c86b22bdcc55cf8c6189fbffa19d0db210
Gabriel Roldan <gabriel.roldan@gmail.com>
2022-02-17 16:36:00 -0300

    Add an X-gs-cloud-instance-id response header if geoserver.debug.instanceId=true

---
+++ starters/starter-spring-boot/src/main/java/org/geoserver/cloud/app/ServiceIdFilterAutoConfiguration.java
@@ -0,0 +1,61 @@
+/*
+ * (c) 2020 Open Source Geospatial Foundation - all rights reserved This code is licensed under the
+ * GPL 2.0 license, available at the root application directory.
+ */
+package org.geoserver.cloud.app;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+/**
+ * Auto configuration to log basic application info at {@link ApplicationReadyEvent app startup}
+ *
+ * <p>Expects the following properties be present in the {@link Environment}: {@literal
+ * spring.application.name}, {@literal info.instance-id}.
+ *
+ * @since 1.0
+ */
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnWebApplication
+@ConditionalOnClass(javax.servlet.Filter.class)
+@ConditionalOnProperty(
+    name = "geoserver.debug.instanceId",
+    havingValue = "true",
+    matchIfMissing = false
+)
+public class ServiceIdFilterAutoConfiguration {
+
+    static class ServiceIdFilter implements javax.servlet.Filter {
+
+        @Value("${info.instance-id:}")
+        String instanceId;
+
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+                throws IOException, ServletException {
+
+            if (response instanceof HttpServletResponse) {
+                HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+                httpServletResponse.addHeader("X-gs-cloud-service-id", instanceId);
+            }
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Bean
+    ServiceIdFilter serviceIdFilter() {
+        return new ServiceIdFilter();
+    }
+}
---
+++ starters/starter-spring-boot/src/test/java/org/geoserver/cloud/app/ServiceIdFilterAutoConfigurationTest.java
@@ -0,0 +1,74 @@
+/*
+ * (c) 2022 Open Source Geospatial Foundation - all rights reserved This code is licensed under the
+ * GPL 2.0 license, available at the root application directory.
+ */
+package org.geoserver.cloud.app;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.FilteredClassLoader;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.boot.test.context.runner.WebApplicationContextRunner;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+/** @since 1.0 */
+class ServiceIdFilterAutoConfigurationTest {
+
+    WebApplicationContextRunner webappRunner =
+            new WebApplicationContextRunner()
+                    .withConfiguration(
+                            AutoConfigurations.of(ServiceIdFilterAutoConfiguration.class))
+                    .withPropertyValues("info.instance-id=test-instance-id");
+
+    @Test
+    void testDisabledByDefault() {
+        webappRunner.run(context -> assertThat(context).doesNotHaveBean("serviceIdFilter"));
+    }
+
+    @Test
+    void testConditionalOnClassServletFilterUnmet() {
+        webappRunner
+                .withPropertyValues("geoserver.debug.instanceId=true")
+                .withClassLoader(new FilteredClassLoader(Filter.class))
+                .run(context -> assertThat(context).doesNotHaveBean("serviceIdFilter"));
+    }
+
+    @Test
+    void testOnWebApplicationConditionUnmet() {
+        ApplicationContextRunner notAWebAppRunner =
+                new ApplicationContextRunner()
+                        .withConfiguration(
+                                AutoConfigurations.of(ServiceIdFilterAutoConfiguration.class))
+                        .withPropertyValues("geoserver.debug.instanceId=true");
+
+        notAWebAppRunner.run(context -> assertThat(context).doesNotHaveBean("serviceIdFilter"));
+    }
+
+    @Test
+    void testEnabledAndAllConditionalsMet() {
+        webappRunner
+                .withPropertyValues( //
+                        "geoserver.debug.instanceId=true")
+                .run(
+                        context -> {
+                            assertThat(context).hasBean("serviceIdFilter");
+                            Filter servletFilter = context.getBean("serviceIdFilter", Filter.class);
+                            ServletRequest req = new MockHttpServletRequest();
+                            MockHttpServletResponse resp = new MockHttpServletResponse();
+                            FilterChain chain = mock(FilterChain.class);
+                            servletFilter.doFilter(req, resp, chain);
+                            assertEquals(
+                                    "test-instance-id", resp.getHeader("X-gs-cloud-service-id"));
+                            verify(chain).doFilter(req, resp);
+                        });
+    }
+}
