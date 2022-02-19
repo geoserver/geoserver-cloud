98fe09f8f166ec512e1c079afe2952a37fd8459d
Gabriel Roldan <gabriel.roldan@gmail.com>
2022-02-19 19:13:52 -0300

    Add a NativeHint to config-service to include gs_cloud_bootstrap_profiles.yml

--- support-services/config/src/main/java/org/geoserver/cloud/config/ConfigApplication.java
+++ support-services/config/src/main/java/org/geoserver/cloud/config/ConfigApplication.java
@@ -7,11 +7,27 @@ package org.geoserver.cloud.config;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.ResourceHint;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableRetry
+@NativeHint(
+    // these type hints are not needed since we've configured config-service not to fetch the eureka
+    // registry
+    //    types =
+    //            @TypeHint(
+    //                types = {
+    //                    com.netflix.discovery.shared.Application.class, //
+    //                    com.netflix.appinfo.InstanceInfo.class, //
+    //                    com.netflix.appinfo.InstanceInfo.PortWrapper.class, //
+    //                    com.netflix.appinfo.MyDataCenterInfo.class
+    //                }
+    //            ), //
+    resources = @ResourceHint(patterns = "gs_cloud_bootstrap_profiles.yml")
+)
 public class ConfigApplication {
 
     public static void main(String[] args) {
