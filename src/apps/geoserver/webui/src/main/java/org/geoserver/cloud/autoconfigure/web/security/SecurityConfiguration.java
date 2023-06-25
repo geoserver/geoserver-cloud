33558c807e38c356879fee7a95d4e5a55b58bde1
Gabriel Roldan <gabriel.roldan@gmail.com>
2023-06-25 11:40:01 -0300

    add externalized config for logout success url in web-ui

--- src/apps/geoserver/webui/src/main/java/org/geoserver/cloud/autoconfigure/web/security/SecurityConfiguration.java
+++ src/apps/geoserver/webui/src/main/java/org/geoserver/cloud/autoconfigure/web/security/SecurityConfiguration.java
@@ -4,9 +4,25 @@
  */
 package org.geoserver.cloud.autoconfigure.web.security;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 import org.geoserver.cloud.config.factory.FilteringXmlBeanDefinitionReader;
+import org.geoserver.security.filter.GeoServerLogoutFilter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 
 @Configuration(proxyBeanMethods = true)
 @ImportResource( //
@@ -17,4 +33,41 @@ import org.springframework.context.annotation.ImportResource;
             "jar:gs-web-sec-ldap-.*!/applicationContext.xml" //
         } //
         )
-public class SecurityConfiguration {}
+public class SecurityConfiguration {
+
+    //	@Bean
+    //	@ConditionalOnProperty("geoserver.web-ui.security.logout-url")
+    //	ConfigurableLogoutUrlFilter configurableLogoutUrlFilter(
+    //			@Value("${geoserver.web-ui.security.logout-url}") String logoutUrl) {
+    //	}
+    //
+
+    @Bean
+    @ConditionalOnProperty("geoserver.web-ui.security.logout-url")
+    FilterRegistrationBean<ConfigurableLogoutUrlFilter> configurableLogoutUrlFilterRegistration(
+            @Value("${geoserver.web-ui.security.logout-url}") String logoutUrl) {
+
+        ConfigurableLogoutUrlFilter filter = new ConfigurableLogoutUrlFilter(logoutUrl);
+
+        FilterRegistrationBean<ConfigurableLogoutUrlFilter> registration =
+                new FilterRegistrationBean<>();
+        registration.setFilter(filter);
+        registration.setOrder(1);
+        return registration;
+    }
+
+    @RequiredArgsConstructor
+    private static class ConfigurableLogoutUrlFilter implements Filter {
+
+        private final @NonNull String logoutUrl;
+
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+                throws IOException, ServletException {
+
+            request.setAttribute(GeoServerLogoutFilter.LOGOUT_REDIRECT_ATTR, logoutUrl);
+
+            chain.doFilter(request, response);
+        }
+    }
+}
