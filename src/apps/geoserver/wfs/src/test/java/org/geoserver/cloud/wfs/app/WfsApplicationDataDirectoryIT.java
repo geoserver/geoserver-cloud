0eafa66151a8960507f23297a06d63307aeca920
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2023-12-21 23:17:07 -0300

    pgconfig catalog backend wfs-service integration test

---
+++ src/apps/geoserver/wfs/src/test/java/org/geoserver/cloud/wfs/app/WfsApplicationDataDirectoryIT.java
@@ -0,0 +1,30 @@
+/*
+ * (c) 2020 Open Source Geospatial Foundation - all rights reserved This code is licensed under the
+ * GPL 2.0 license, available at the root application directory.
+ */
+package org.geoserver.cloud.wfs.app;
+
+import org.junit.jupiter.api.io.TempDir;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@SpringBootTest(classes = WfsApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("datadir")
+class WfsApplicationDataDirectoryIT extends WfsApplicationTest {
+
+    static @TempDir Path tmpdir;
+    static Path datadir;
+
+    @DynamicPropertySource
+    static void setUpDataDir(DynamicPropertyRegistry registry) throws IOException {
+        datadir = Files.createDirectory(tmpdir.resolve("datadir"));
+        registry.add("geoserver.backend.data-directory.location", datadir::toAbsolutePath);
+    }
+}
