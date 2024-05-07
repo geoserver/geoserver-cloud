69e5207ca17faf9a6d2c282774f09f58d86900dd
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2024-05-07 00:47:06 -0300

    share webui authentication with the other services through the gateway
    Introduce a `GatewaySharedAuthenticationFilter`, where the web-ui
    service acts as service, returning the authenticated user name and roles
    as response headers, the other services act as client, receiving the
    user name and roles as request headers, and the gateway acts as
    intermediary to convey the auth returned by the webui and sending it to
    the other services.
    
    The functionality is enabled by default, and automatically creates the
    required authentication filter and injects it to the filter chains.
    
    This behavior can be controlled through the following configuration
    properties:
    
    ```
      geoserver:
        security:
          gateway-shared-auth:
             enabled: true
             auto: true
             server: false (true for webui)
    ```

---
+++ src/starters/security/src/main/java/org/geoserver/cloud/security/gateway/sharedauth/GatewaySharedAuthenticationInitializer.java
@@ -0,0 +1,147 @@
+/*
+ * (c) 2023 Open Source Geospatial Foundation - all rights reserved This code is licensed under the
+ * GPL 2.0 license, available at the root application directory.
+ */
+package org.geoserver.cloud.security.gateway.sharedauth;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.geoserver.cloud.security.gateway.sharedauth.GatewaySharedAuthenticationFilter.Config;
+import org.geoserver.platform.ContextLoadedEvent;
+import org.geoserver.platform.resource.Resource;
+import org.geoserver.platform.resource.Resource.Lock;
+import org.geoserver.security.GeoServerSecurityFilterChain;
+import org.geoserver.security.GeoServerSecurityManager;
+import org.geoserver.security.RequestFilterChain;
+import org.geoserver.security.config.SecurityManagerConfig;
+import org.geoserver.security.validation.SecurityConfigException;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.Ordered;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Upon a GeoServer {@link ContextLoadedEvent}, creates the {@link
+ * GatewaySharedAuthenticationFilter} filter in the {@link GeoServerSecurityManager} configuration,
+ * and appends the filter to the {@link SecurityManagerConfig#getFilterChain() filter chain}.
+ *
+ * @since 1.9
+ */
+@Slf4j(topic = "org.geoserver.cloud.security.gateway.sharedauth")
+@RequiredArgsConstructor
+public class GatewaySharedAuthenticationInitializer
+        implements ApplicationListener<ContextLoadedEvent>, Ordered {
+
+    @NonNull private final GeoServerSecurityManager securityManager;
+
+    @Override
+    public void onApplicationEvent(ContextLoadedEvent event) {
+        if (!filterIsMissing()) {
+            log.info(
+                    "{} config is present.",
+                    GatewaySharedAuthenticationFilter.class.getSimpleName());
+            return;
+        }
+        log.info(
+                "{} config is missing, acquiring security lock",
+                GatewaySharedAuthenticationFilter.class.getSimpleName());
+        final Resource security = securityManager.security();
+        final Lock securityLock = security.lock();
+        try {
+            if (filterIsMissing()) {
+                Config filterConfig = createFilter();
+                addToFilterChains(filterConfig);
+            } else {
+                log.info(
+                        "{} config already present after acquiring security lock",
+                        GatewaySharedAuthenticationFilter.class.getSimpleName());
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            securityLock.release();
+        }
+    }
+
+    private void addToFilterChains(Config filterConfig) throws Exception {
+        final String filterName = filterConfig.getName();
+        SecurityManagerConfig config = securityManager.getSecurityConfig();
+        GeoServerSecurityFilterChain filterChain = config.getFilterChain();
+
+        final Set<String> applyTo = Set.of("web", "rest", "gwc", "default");
+
+        filterChain
+                .getRequestChains()
+                .forEach(
+                        requestFiltersChain -> {
+                            String chainName = requestFiltersChain.getName();
+                            if (applyTo.contains(chainName)) {
+                                addToFilterChain(requestFiltersChain, filterName);
+                            }
+                        });
+
+        securityManager.saveSecurityConfig(config);
+        log.info("SecurityManagerConfig saved");
+    }
+
+    private void addToFilterChain(RequestFilterChain requestFiltersChain, String filterName) {
+        List<String> filterNames = new ArrayList<>(requestFiltersChain.getFilterNames());
+        if (filterNames.contains(filterName)) {
+            log.info(
+                    "Filter chain {} already contains {} -> {}",
+                    requestFiltersChain.getName(),
+                    filterName,
+                    requestFiltersChain.getFilterNames().stream()
+                            .collect(Collectors.joining(", ")));
+        } else {
+            if (filterNames.contains("anonymous")) {
+                // anonymous must always be the last one if present
+                int index = filterNames.indexOf("anonymous");
+                filterNames.add(index, filterName);
+            } else {
+                filterNames.add(filterName);
+            }
+
+            requestFiltersChain.setFilterNames(filterNames);
+            log.info(
+                    "Applied filter {} to filter chain {} -> {}",
+                    filterName,
+                    requestFiltersChain.getName(),
+                    requestFiltersChain.getFilterNames().stream()
+                            .collect(Collectors.joining(", ")));
+        }
+    }
+
+    private boolean filterIsMissing() {
+        try {
+            return securityManager.listFilters(GatewaySharedAuthenticationFilter.class).isEmpty();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private Config createFilter() throws SecurityConfigException, IOException {
+        var config = new GatewaySharedAuthenticationFilter.Config();
+        securityManager.saveFilter(config);
+        log.info("Created {}", GatewaySharedAuthenticationFilter.class.getSimpleName());
+        return config;
+    }
+
+    /**
+     * @return {@link Ordered#LOWEST_PRECEDENCE} to make sure it runs after {@link
+     *     GeoServerSecurityManager#onApplicationEvent}
+     */
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
+    }
+}
---
+++ src/starters/security/src/main/java/org/geoserver/cloud/security/gateway/sharedauth/ServerConfiguration.java
@@ -0,0 +1,65 @@
+/*
+ * (c) 2024 Open Source Geospatial Foundation - all rights reserved This code is licensed under the
+ * GPL 2.0 license, available at the root application directory.
+ */
+package org.geoserver.cloud.security.gateway.sharedauth;
+
+import static org.geoserver.cloud.security.gateway.sharedauth.GatewaySharedAuthenticationProvider.Mode.SERVER;
+
+import org.apache.wicket.model.IModel;
+import org.geoserver.security.web.auth.AuthenticationFilterPanel;
+import org.geoserver.security.web.auth.AuthenticationFilterPanelInfo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Contributes a {@link GatewaySharedAuthenticationProvider} in server mode.
+ *
+ * @see ClientConfiguration
+ * @since 1.9
+ */
+@Configuration
+public class ServerConfiguration {
+
+    @Bean
+    GatewaySharedAuthenticationProvider gatewaySharedAuthenticationProvider() {
+        return new GatewaySharedAuthenticationProvider(SERVER);
+    }
+
+    @Bean
+    GatewaySharedAuthFilterPanelInfo gatewaySharedAuthPanelInfo() {
+        var panelInfo = new GatewaySharedAuthFilterPanelInfo();
+        panelInfo.setId("security.gatewaySharedAuthFilter");
+        panelInfo.setShortTitleKey("GatewaySharedAuthFilterPanel.short");
+        panelInfo.setTitleKey("GatewaySharedAuthFilterPanel.title");
+        panelInfo.setDescriptionKey("GatewaySharedAuthFilterPanel.description");
+
+        panelInfo.setComponentClass(GatewaySharedAuthAuthFilterPanel.class);
+        panelInfo.setServiceClass(GatewaySharedAuthenticationFilter.class);
+        panelInfo.setServiceConfigClass(GatewaySharedAuthenticationFilter.Config.class);
+
+        return panelInfo;
+    }
+
+    @SuppressWarnings("serial")
+    public static class GatewaySharedAuthFilterPanelInfo
+            extends AuthenticationFilterPanelInfo<
+                    GatewaySharedAuthenticationFilter.Config, GatewaySharedAuthAuthFilterPanel> {
+
+        public GatewaySharedAuthFilterPanelInfo() {
+            setServiceClass(GatewaySharedAuthenticationFilter.class);
+            setServiceConfigClass(GatewaySharedAuthenticationFilter.Config.class);
+            setComponentClass(GatewaySharedAuthAuthFilterPanel.class);
+        }
+    }
+
+    @SuppressWarnings({"serial", "java:S110"})
+    public static class GatewaySharedAuthAuthFilterPanel
+            extends AuthenticationFilterPanel<GatewaySharedAuthenticationFilter.Config> {
+
+        public GatewaySharedAuthAuthFilterPanel(
+                String id, IModel<GatewaySharedAuthenticationFilter.Config> model) {
+            super(id, model);
+        }
+    }
+}
