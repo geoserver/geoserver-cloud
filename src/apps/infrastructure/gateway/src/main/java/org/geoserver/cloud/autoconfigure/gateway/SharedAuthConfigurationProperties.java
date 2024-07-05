00d7f7278eb8a6faa47c8969809338f6b4250002
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2024-07-05 17:11:07 -0300

    Allow to disable gateway-shared-auth global filter in the gateway
    The config property `geoserver.security.gateway-shared-auth.enabled`
    only allowed to disable the gateway/webui shared auth in the gs
    services.
    
    This patch makes it possible to also disable the global filter in the
    gateway.

---
+++ src/apps/infrastructure/gateway/src/main/java/org/geoserver/cloud/autoconfigure/gateway/SharedAuthConfigurationProperties.java
@@ -0,0 +1,25 @@
+/*
+ * (c) 2024 Open Source Geospatial Foundation - all rights reserved This code is licensed under the
+ * GPL 2.0 license, available at the root application directory.
+ */
+package org.geoserver.cloud.autoconfigure.gateway;
+
+import lombok.Data;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * For automatic documentation purposes only, as used by the {@literal
+ * spring-boot-configuration-processor}
+ */
+@ConfigurationProperties(prefix = "geoserver.security.gateway-shared-auth")
+@Data
+class SharedAuthConfigurationProperties {
+
+    /**
+     * Enable or disable the Gateway/WebUI Shared Authentication mechanism, where the Gateway works
+     * as mediator to share the authentication from the GeoServer WebUI with the rest of the
+     * services.
+     */
+    private boolean enabled = true;
+}
---
+++ src/apps/infrastructure/gateway/src/test/java/org/geoserver/cloud/autoconfigure/gateway/GatewayApplicationAutoconfigurationTest.java
@@ -0,0 +1,46 @@
+/*
+ * (c) 2020 Open Source Geospatial Foundation - all rights reserved This code is licensed under the
+ * GPL 2.0 license, available at the root application directory.
+ */
+package org.geoserver.cloud.autoconfigure.gateway;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.geoserver.cloud.gateway.filter.GatewaySharedAuhenticationGlobalFilter;
+import org.geoserver.cloud.gateway.filter.RouteProfileGatewayFilterFactory;
+import org.geoserver.cloud.gateway.filter.StripBasePathGatewayFilterFactory;
+import org.geoserver.cloud.gateway.predicate.RegExpQueryRoutePredicateFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ReactiveWebApplicationContextRunner;
+
+class GatewayApplicationAutoconfigurationTest {
+
+    private ReactiveWebApplicationContextRunner runner =
+            new ReactiveWebApplicationContextRunner()
+                    .withConfiguration(
+                            AutoConfigurations.of(GatewayApplicationAutoconfiguration.class));
+
+    @Test
+    void testDefaultAppContextContributions() {
+        runner.run(
+                context ->
+                        assertThat(context)
+                                .hasNotFailed()
+                                .hasSingleBean(RegExpQueryRoutePredicateFactory.class)
+                                .hasSingleBean(RouteProfileGatewayFilterFactory.class)
+                                .hasSingleBean(StripBasePathGatewayFilterFactory.class)
+                                .hasSingleBean(GatewaySharedAuhenticationGlobalFilter.class));
+    }
+
+    @Test
+    void disableGatewaySharedAuhenticationGlobalFilter() {
+        runner.withPropertyValues("geoserver.security.gateway-shared-auth.enabled: false")
+                .run(
+                        context ->
+                                assertThat(context)
+                                        .hasNotFailed()
+                                        .doesNotHaveBean(
+                                                GatewaySharedAuhenticationGlobalFilter.class));
+    }
+}
