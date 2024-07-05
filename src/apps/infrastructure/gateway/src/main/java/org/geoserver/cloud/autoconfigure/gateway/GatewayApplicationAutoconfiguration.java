1ec5e266818325bec4e76b1496b5ffd96d3caee2
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2024-07-05 17:11:07 -0300

    Allow to disable gateway-shared-auth global filter in the gateway
    The config property `geoserver.security.gateway-shared-auth.enabled`
    only allowed to disable the gateway/webui shared auth in the gs
    services.
    
    This patch makes it possible to also disable the global filter in the
    gateway.

---
+++ src/apps/infrastructure/gateway/src/main/java/org/geoserver/cloud/autoconfigure/gateway/GatewayApplicationAutoconfiguration.java
@@ -0,0 +1,79 @@
+/*
+ * (c) 2020 Open Source Geospatial Foundation - all rights reserved This code is licensed under the
+ * GPL 2.0 license, available at the root application directory.
+ */
+package org.geoserver.cloud.autoconfigure.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.geoserver.cloud.gateway.filter.GatewaySharedAuhenticationGlobalFilter;
+import org.geoserver.cloud.gateway.filter.RouteProfileGatewayFilterFactory;
+import org.geoserver.cloud.gateway.filter.StripBasePathGatewayFilterFactory;
+import org.geoserver.cloud.gateway.predicate.RegExpQueryRoutePredicateFactory;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+@AutoConfiguration
+@Slf4j
+public class GatewayApplicationAutoconfiguration {
+
+    /**
+     * Custom gateway predicate factory to support matching by regular expressions on both name and
+     * value of query parameters
+     *
+     * <p>E.g.:
+     *
+     * <pre>{@code
+     * - id: wms_ows
+     *   uri: http://wms-service:8080
+     *   predicates:
+     *     # match service=wms case insensitively
+     *     - RegExpQuery=(?i:service),(?i:wms)
+     * }</pre>
+     */
+    @Bean
+    RegExpQueryRoutePredicateFactory regExpQueryRoutePredicateFactory() {
+        return new RegExpQueryRoutePredicateFactory();
+    }
+
+    /**
+     * Allows to enable routes only if a given spring profile is enabled
+     *
+     * <p>Since the `spring.cloud.gateway.routes` is a list and not a map/dictionary, routes can't
+     * be added in profiles, because the list is overritten fully. This filter allows to enable
+     * routes based on profiles from a single list of routes.
+     *
+     * <p>E.g.:
+     *
+     * <pre>{@code
+     * - id: catalog
+     *   uri: ...
+     *   predicates:
+     *     - Path=${geoserver.base-path}/api/v1/**
+     *   filters:
+     *     # Expose the catalog and configuration API only if the dev profile is active
+     *     - RouteProfile=dev,403
+     * }</pre>
+     */
+    @Bean
+    RouteProfileGatewayFilterFactory routeProfileGatewayFilterFactory(Environment environment) {
+        return new RouteProfileGatewayFilterFactory(environment);
+    }
+
+    @Bean
+    StripBasePathGatewayFilterFactory stripBasePathGatewayFilterFactory() {
+        return new StripBasePathGatewayFilterFactory();
+    }
+
+    @Bean
+    @ConditionalOnProperty(
+            name = "geoserver.security.gateway-shared-auth.enabled",
+            havingValue = "true",
+            matchIfMissing = true)
+    GatewaySharedAuhenticationGlobalFilter gatewaySharedAuhenticationGlobalFilter() {
+        log.info("gateway-shared-auth is enabled");
+        return new GatewaySharedAuhenticationGlobalFilter();
+    }
+}
--- src/apps/infrastructure/gateway/src/main/java/org/geoserver/cloud/gateway/GatewayApplication.java
+++ src/apps/infrastructure/gateway/src/main/java/org/geoserver/cloud/gateway/GatewayApplication.java
@@ -4,53 +4,19 @@
  */
 package org.geoserver.cloud.gateway;
 
-import org.geoserver.cloud.gateway.filter.GatewaySharedAuhenticationGlobalFilter;
-import org.geoserver.cloud.gateway.filter.RouteProfileGatewayFilterFactory;
-import org.geoserver.cloud.gateway.filter.StripBasePathGatewayFilterFactory;
-import org.geoserver.cloud.gateway.predicate.RegExpQueryRoutePredicateFactory;
+import org.geoserver.cloud.autoconfigure.gateway.GatewayApplicationAutoconfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
+/**
+ * Spring Cloud Gateway application for GeoServer Cloud
+ *
+ * @see GatewayApplicationAutoconfiguration
+ */
 @SpringBootApplication
-@Configuration(proxyBeanMethods = false)
 public class GatewayApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(GatewayApplication.class).run(args);
-    }
-
-    @Bean
-    RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes().build();
-    }
-
-    /**
-     * Custom gateway predicate factory to support matching by regular expressions on both name and
-     * value of query parameters
-     */
-    @Bean
-    RegExpQueryRoutePredicateFactory regExpQueryRoutePredicateFactory() {
-        return new RegExpQueryRoutePredicateFactory();
-    }
-
-    /** Allows to enable routes only if a given spring profile is enabled */
-    @Bean
-    RouteProfileGatewayFilterFactory routeProfileGatewayFilterFactory(Environment environment) {
-        return new RouteProfileGatewayFilterFactory(environment);
-    }
-
-    @Bean
-    StripBasePathGatewayFilterFactory stripBasePathGatewayFilterFactory() {
-        return new StripBasePathGatewayFilterFactory();
-    }
-
-    @Bean
-    GatewaySharedAuhenticationGlobalFilter gatewaySharedAuhenticationGlobalFilter() {
-        return new GatewaySharedAuhenticationGlobalFilter();
     }
 }
