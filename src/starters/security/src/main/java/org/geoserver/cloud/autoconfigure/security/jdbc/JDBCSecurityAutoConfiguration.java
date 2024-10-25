5dcd199a928dbb3acd482312b518dd2f1a0aab25
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2024-10-25 11:11:32 -0300

    Add JDBC and LDAP security plugins
    Both plugins are enabled by default, can be disabled with
    
    ```
    geoserver.security.ldap=false
    geoserver.security.jdbc=false
    ```

---
+++ src/starters/security/src/main/java/org/geoserver/cloud/autoconfigure/security/jdbc/JDBCSecurityAutoConfiguration.java
@@ -0,0 +1,28 @@
+/*
+ * (c) 2024 Open Source Geospatial Foundation - all rights reserved This code is licensed under the
+ * GPL 2.0 license, available at the root application directory.
+ */
+package org.geoserver.cloud.autoconfigure.security.jdbc;
+
+import org.geoserver.cloud.autoconfigure.security.ConditionalOnGeoServerSecurityEnabled;
+import org.geoserver.cloud.autoconfigure.security.GeoServerSecurityAutoConfiguration;
+import org.geoserver.cloud.config.factory.FilteringXmlBeanDefinitionReader;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ImportResource;
+
+/** {@link AutoConfiguration @AutoConfiguration} to enable {@code gs-sec-jdbc} */
+// run before GeoServerSecurityAutoConfiguration so the provider is available when
+// GeoServerSecurityManager calls GeoServerExtensions.extensions(GeoServerSecurityProvider.class)
+@AutoConfiguration(before = GeoServerSecurityAutoConfiguration.class)
+@EnableConfigurationProperties(JDBCSecurityConfigProperties.class)
+@ConditionalOnGeoServerSecurityEnabled
+@ConditionalOnProperty(
+        name = "geoserver.security.jdbc",
+        havingValue = "true",
+        matchIfMissing = true)
+@ImportResource(
+        reader = FilteringXmlBeanDefinitionReader.class, //
+        locations = "jar:gs-sec-jdbc-.*!/applicationContext.xml")
+public class JDBCSecurityAutoConfiguration {}
---
+++ src/starters/security/src/test/java/org/geoserver/cloud/autoconfigure/security/jdbc/JDBCSecurityWebUIAutoConfigurationTest.java
@@ -0,0 +1,72 @@
+/*
+ * (c) 2024 Open Source Geospatial Foundation - all rights reserved This code is licensed under the
+ * GPL 2.0 license, available at the root application directory.
+ */
+package org.geoserver.cloud.autoconfigure.security.jdbc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+import org.geoserver.platform.ModuleStatusImpl;
+import org.geoserver.security.GeoServerSecurityManager;
+import org.geoserver.security.web.auth.AuthenticationFilterPanelInfo;
+import org.geoserver.security.web.jdbc.JDBCAuthProviderPanelInfo;
+import org.geoserver.security.web.jdbc.JDBCRoleServicePanelInfo;
+import org.geoserver.security.web.jdbc.JDBCUserGroupServicePanelInfo;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.FilteredClassLoader;
+import org.springframework.boot.test.context.runner.WebApplicationContextRunner;
+
+class JDBCSecurityWebUIAutoConfigurationTest {
+
+    private WebApplicationContextRunner runner =
+            new WebApplicationContextRunner()
+                    .withConfiguration(
+                            AutoConfigurations.of(
+                                    JDBCSecurityAutoConfiguration.class,
+                                    JDBCSecurityWebUIAutoConfiguration.class))
+                    .withBean(
+                            GeoServerSecurityManager.class,
+                            () -> mock(GeoServerSecurityManager.class));
+
+    @Test
+    void testConditionalOnClassNoMatch() {
+        runner.withClassLoader(new FilteredClassLoader(AuthenticationFilterPanelInfo.class))
+                .run(
+                        context ->
+                                assertThat(context)
+                                        .hasNotFailed()
+                                        .doesNotHaveBean(JDBCUserGroupServicePanelInfo.class)
+                                        .doesNotHaveBean(JDBCRoleServicePanelInfo.class)
+                                        .doesNotHaveBean(JDBCAuthProviderPanelInfo.class)
+                                        .doesNotHaveBean("jdbcSecurityWebExtension"));
+    }
+
+    @Test
+    void testConditionalOnClassMatch() {
+        runner.run(
+                context ->
+                        assertThat(context)
+                                .hasNotFailed()
+                                .hasSingleBean(JDBCUserGroupServicePanelInfo.class)
+                                .hasSingleBean(JDBCRoleServicePanelInfo.class)
+                                .hasSingleBean(JDBCAuthProviderPanelInfo.class)
+                                .hasBean("jdbcSecurityWebExtension")
+                                .getBean("jdbcSecurityWebExtension")
+                                .isInstanceOf(ModuleStatusImpl.class));
+    }
+
+    @Test
+    void testDisabled() {
+        runner.withPropertyValues("geoserver.security.jdbc=false")
+                .run(
+                        context ->
+                                assertThat(context)
+                                        .hasNotFailed()
+                                        .doesNotHaveBean(JDBCUserGroupServicePanelInfo.class)
+                                        .doesNotHaveBean(JDBCRoleServicePanelInfo.class)
+                                        .doesNotHaveBean(JDBCAuthProviderPanelInfo.class)
+                                        .doesNotHaveBean("jdbcSecurityWebExtension"));
+    }
+}
