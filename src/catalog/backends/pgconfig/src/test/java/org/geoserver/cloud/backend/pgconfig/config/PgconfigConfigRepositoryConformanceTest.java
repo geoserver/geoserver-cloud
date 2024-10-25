33dfdc50d7680a9abcda3f3e6c6e083eb6b58bbb
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2024-10-25 19:30:22 -0300

    Fix saving workspace services not working with pgconfig
    Saving the workspace services not working with pgconfig when
    caching is enabled. `CachingGeoServerFacade` assumed there's
    nothing to do when a `ServiceInfo` is added for a workspace,
    but it was caching the `null` value, which is expected to save
    db roundtrips.
    
    - Add missing treatment of `ServiceInfoAdded` and `SettingInfoAdded`
    to `CachingGeoServerFacade`.
    
    - Add referential integrity for settingsinfo and serviceinfo
    
    Now the flyway migration will add referential integrity for:
    
     - `settingsinfo(workspace) -> workspaceindo(id)`
     - `serviceinfo(workspace) -> workspaceinfo(id)`
    
    And unique indexes for:
    
    - `settingsinfo(workspace)`
    - `serviceinfo("@type", workspace)`
    
    As a side effect of the caching facade returning null once a
    `ServiceInfo` as added, you could add multiple services of the
    same kind for a workspace. The referential integrity checks will
    avoid that, and the FlyWay migration will delete existing duplicates.

--- src/catalog/backends/pgconfig/src/test/java/org/geoserver/cloud/backend/pgconfig/config/PgconfigConfigRepositoryConformanceTest.java
+++ src/catalog/backends/pgconfig/src/test/java/org/geoserver/cloud/backend/pgconfig/config/PgconfigConfigRepositoryConformanceTest.java
@@ -4,13 +4,19 @@
  */
 package org.geoserver.cloud.backend.pgconfig.config;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.WorkspaceInfo;
 import org.geoserver.cloud.backend.pgconfig.PgconfigBackendBuilder;
 import org.geoserver.cloud.backend.pgconfig.support.PgConfigTestContainer;
 import org.geoserver.config.GeoServer;
 import org.geoserver.config.GeoServerConfigConformanceTest;
+import org.geoserver.config.ServiceInfo;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -18,7 +24,6 @@ import org.testcontainers.junit.jupiter.Testcontainers;
  * @since 1.4
  */
 @Testcontainers(disabledWithoutDocker = true)
-@SuppressWarnings("java:S2187")
 class PgconfigConfigRepositoryConformanceTest extends GeoServerConfigConformanceTest {
 
     @Container static PgConfigTestContainer<?> container = new PgConfigTestContainer<>();
@@ -39,5 +44,33 @@ class PgconfigConfigRepositoryConformanceTest extends GeoServerConfigConformance
         PgconfigBackendBuilder builder = new PgconfigBackendBuilder(container.getDataSource());
         Catalog catalog = builder.createCatalog();
         return builder.createGeoServer(catalog);
+    }
+
+    @Test
+    void testAddDuplicateServiceToWorkspace() {
+        // Make a workspace
+        WorkspaceInfo ws1 = geoServer.getCatalog().getFactory().createWorkspace();
+        ws1.setName("TEST-WORKSPACE-1");
+        geoServer.getCatalog().add(ws1);
+
+        // Make a service for that workspace
+        ServiceInfo service1 = createService();
+        service1.setWorkspace(ws1);
+        service1.setName("TEST-OWS");
+        service1.setTitle("Service for WS1");
+        geoServer.add(service1);
+
+        ServiceInfo dupTypeAndWorkspace = createService();
+        dupTypeAndWorkspace.setWorkspace(ws1);
+        dupTypeAndWorkspace.setName("TEST-OWS");
+        dupTypeAndWorkspace.setTitle("Service for WS1");
+
+        var ex =
+                assertThrows(
+                        IllegalArgumentException.class, () -> geoServer.add(dupTypeAndWorkspace));
+
+        assertThat(ex.getMessage())
+                .contains(
+                        "service with name 'TEST-OWS' already exists in workspace 'TEST-WORKSPACE-1'");
     }
 }
