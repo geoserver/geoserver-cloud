c422df105b7f3df596a813d7632cfdef2a31e44e
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2024-10-25 23:37:18 -0300

    pgconfig: also delete duplicate settingsinfos

--- src/catalog/backends/pgconfig/src/main/resources/db/pgconfig/migration/postgresql/V1_8__ServiceInfoSettingsInfoConstraints.sql
+++ src/catalog/backends/pgconfig/src/main/resources/db/pgconfig/migration/postgresql/V1_8__ServiceInfoSettingsInfoConstraints.sql
@@ -10,6 +10,27 @@
  * - serviceinfo("@type", workspace)
  */
 
+/*
+ * Delete possible duplicate settingsinfo by workspace before enforcing uniqueness.
+ */
+DELETE FROM settingsinfo WHERE id IN( SELECT id FROM (
+  SELECT id,
+  ROW_NUMBER() OVER(PARTITION BY workspace ORDER BY id ASC) AS duprow
+  FROM settingsinfo
+) dups
+WHERE
+dups.duprow > 1);
+
+/*
+ * Delete possible duplicate serviceinfos by type and workspace before enforcing uniqueness.
+ */
+DELETE FROM serviceinfo WHERE id IN( SELECT id FROM (
+  SELECT id,
+  ROW_NUMBER() OVER(PARTITION BY "@type", workspace ORDER BY id ASC) AS duprow
+  FROM serviceinfo
+) dups
+WHERE
+dups.duprow > 1);
 
 ALTER TABLE settingsinfo
   ADD CONSTRAINT fk_settingsinfo_workspace
@@ -20,17 +41,6 @@ ALTER TABLE settingsinfo
 CREATE UNIQUE INDEX settingsinfo_workspace_key
   ON settingsinfo (workspace)
   NULLS NOT DISTINCT;
-
-/*
- * Delete possible duplicate serviceinfos by type and workspace before enforcing uniqueness.
- */
-DELETE FROM serviceinfo WHERE id IN( select id FROM (
-  SELECT id,
-  ROW_NUMBER() OVER(PARTITION BY "@type", workspace ORDER BY id ASC) AS duprow
-  FROM serviceinfo
-) dups
-WHERE
-dups.duprow > 1);
 
 ALTER TABLE serviceinfo
   ADD CONSTRAINT fk_serviceinfo_workspace
