59780d994c3cefe367653f7b1a472f367ac77d13
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2025-07-30 23:17:51 -0300

    move gs-vectortiles configuration to gs-spring-configuration

---
+++ src/geoserver-configuration/src/main/java/org/geoserver/configuration/extension/vectortiles/VectorTilesMapBoxConfiguration.java
@@ -0,0 +1,19 @@
+/* (c) 2025 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+
+package org.geoserver.configuration.extension.vectortiles;
+
+import org.geoserver.cloud.config.factory.ImportFilteredResource;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration for MapBox Vector Tiles format.
+ *
+ * @since 2.27.0
+ */
+@Configuration
+@ImportFilteredResource(
+        "jar:gs-vectortiles-.*!/applicationContext.xml#name=(wmsMapBoxBuilderFactory|wmsMapBoxMapOutputFormat)")
+public class VectorTilesMapBoxConfiguration {}
