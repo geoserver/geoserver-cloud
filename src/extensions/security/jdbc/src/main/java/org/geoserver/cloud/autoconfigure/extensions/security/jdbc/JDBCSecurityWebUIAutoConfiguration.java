997c946bff139c3e7d919825559ed30194b595f8
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2025-07-30 22:35:16 -0300

    move sec-jdbc configuration to gs-spring-configuration

--- src/extensions/security/jdbc/src/main/java/org/geoserver/cloud/autoconfigure/extensions/security/jdbc/JDBCSecurityWebUIAutoConfiguration.java
+++ src/extensions/security/jdbc/src/main/java/org/geoserver/cloud/autoconfigure/extensions/security/jdbc/JDBCSecurityWebUIAutoConfiguration.java
@@ -5,28 +5,34 @@
 
 package org.geoserver.cloud.autoconfigure.extensions.security.jdbc;
 
-import org.geoserver.cloud.config.factory.ImportFilteredResource;
+import org.geoserver.configuration.core.security.jdbc.JDBCSecurityWebUIConfiguration;
 import org.geoserver.security.web.auth.AuthenticationFilterPanelInfo;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
 
 /**
  * Auto-configuration for the GeoServer JDBC security web UI components.
  *
- * <p>This configuration is only active when:
+ * <p>
+ * This configuration is only active when:
  * <ul>
- *   <li>The JDBC security extension is enabled
- *   <li>The GeoServer web UI classes are available on the classpath
+ * <li>The JDBC security extension is enabled
+ * <li>The GeoServer web UI classes are available on the classpath
  * </ul>
  *
- * <p>It registers the JDBC security web UI components like panel info classes
- * for configuration through the GeoServer web admin interface.
+ * <p>
+ * It registers the JDBC security web UI components like panel info classes for
+ * configuration through the GeoServer web admin interface.
  *
  * @since 2.27.0.0
+ * @see JDBCSecurityWebUIConfiguration
  */
 @AutoConfiguration
-@SuppressWarnings("java:S1118") // Suppress SonarLint warning, constructor needs to be public
 @ConditionalOnJDBC
 @ConditionalOnClass(AuthenticationFilterPanelInfo.class)
-@ImportFilteredResource("jar:gs-web-sec-jdbc-.*!/applicationContext.xml")
+@EnableConfigurationProperties(JDBCConfigProperties.class)
+@Import(JDBCSecurityWebUIConfiguration.class)
+@SuppressWarnings("java:S1118") // Suppress SonarLint warning, constructor needs to be public
 public class JDBCSecurityWebUIAutoConfiguration {}
