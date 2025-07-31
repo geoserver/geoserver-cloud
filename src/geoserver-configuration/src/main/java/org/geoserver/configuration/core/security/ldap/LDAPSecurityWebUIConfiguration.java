ab8f7b46ec5541b9b011aa598fa0a078952655cf
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2025-07-31 00:43:08 -0300

    transpile gs-sec-ldap configuration

--- src/geoserver-configuration/src/main/java/org/geoserver/configuration/core/security/ldap/LDAPSecurityWebUIConfiguration.java
+++ src/geoserver-configuration/src/main/java/org/geoserver/configuration/core/security/ldap/LDAPSecurityWebUIConfiguration.java
@@ -5,7 +5,7 @@
 
 package org.geoserver.configuration.core.security.ldap;
 
-import org.geoserver.cloud.config.factory.ImportFilteredResource;
+import org.geoserver.spring.config.annotations.TranspileXmlConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -19,7 +19,7 @@ import org.springframework.context.annotation.Import;
  * @since 2.27.0.0
  */
 @Configuration
-@Import(LDAPSecurityConfiguration.class)
-@ImportFilteredResource("jar:gs-web-sec-ldap-.*!/applicationContext.xml")
+@TranspileXmlConfig(locations = "jar:gs-web-sec-ldap-.*!/applicationContext.xml")
+@Import({LDAPSecurityConfiguration.class, LDAPSecurityWebUIConfiguration_Generated.class})
 @SuppressWarnings("java:S1118") // Suppress SonarLint warning, constructor needs to be public
 public class LDAPSecurityWebUIConfiguration {}
