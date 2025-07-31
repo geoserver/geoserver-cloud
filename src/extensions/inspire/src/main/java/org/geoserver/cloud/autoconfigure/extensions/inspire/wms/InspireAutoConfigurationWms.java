b37f5d74b32295b0a648ba14961e7f9ae2f969b7
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2025-07-31 02:13:09 -0300

    transpile gs-inspire configuration

--- src/extensions/inspire/src/main/java/org/geoserver/cloud/autoconfigure/extensions/inspire/wms/InspireAutoConfigurationWms.java
+++ src/extensions/inspire/src/main/java/org/geoserver/cloud/autoconfigure/extensions/inspire/wms/InspireAutoConfigurationWms.java
@@ -4,14 +4,13 @@
  */
 package org.geoserver.cloud.autoconfigure.extensions.inspire.wms;
 
-import lombok.extern.slf4j.Slf4j;
 import org.geoserver.configuration.extension.inspire.InspireConfigurationWms;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 @AutoConfiguration
-@SuppressWarnings("java:S1118") // Suppress SonarLint warning, constructor needs to be public
 @Import(InspireConfigurationWms.class)
-@Slf4j(topic = "org.geoserver.cloud.autoconfigure.extensions.inspire.wms")
 @ConditionalOnInspireWms
-public class InspireAutoConfigurationWms {}
+@SuppressWarnings("java:S1118") // Suppress SonarLint warning, constructor needs to be public
+public class InspireAutoConfigurationWms {
+}
