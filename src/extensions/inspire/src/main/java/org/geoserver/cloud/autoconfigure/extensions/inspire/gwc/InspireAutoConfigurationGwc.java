e9e7e07dd13150888f07674323bb7af29acd9c37
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2025-07-31 02:13:09 -0300

    transpile gs-inspire configuration

--- src/extensions/inspire/src/main/java/org/geoserver/cloud/autoconfigure/extensions/inspire/gwc/InspireAutoConfigurationGwc.java
+++ src/extensions/inspire/src/main/java/org/geoserver/cloud/autoconfigure/extensions/inspire/gwc/InspireAutoConfigurationGwc.java
@@ -4,14 +4,15 @@
  */
 package org.geoserver.cloud.autoconfigure.extensions.inspire.gwc;
 
-import lombok.extern.slf4j.Slf4j;
 import org.geoserver.configuration.extension.inspire.InspireConfigurationGwc;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Import;
 
+import lombok.extern.slf4j.Slf4j;
+
 @AutoConfiguration
-@SuppressWarnings("java:S1118") // Suppress SonarLint warning, constructor needs to be public
+@ConditionalOnInspireGwc
 @Import(InspireConfigurationGwc.class)
 @Slf4j(topic = "org.geoserver.cloud.autoconfigure.extensions.inspire.gwc")
-@ConditionalOnInspireGwc
+@SuppressWarnings("java:S1118") // Suppress SonarLint warning, constructor needs to be public
 public class InspireAutoConfigurationGwc {}
--- src/geoserver-configuration/src/main/java/org/geoserver/configuration/extension/inspire/InspireConfigurationWcs.java
+++ src/geoserver-configuration/src/main/java/org/geoserver/configuration/extension/inspire/InspireConfigurationWcs.java
@@ -4,21 +4,22 @@
  */
 package org.geoserver.configuration.extension.inspire;
 
-import org.geoserver.cloud.config.factory.ImportFilteredResource;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.geoserver.spring.config.annotations.TranspileXmlConfig;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * Configuration class that sets up the INSPIRE extension components.
  *
- * <p>This configuration is only active when the INSPIRE extension is enabled
+ * <p>
+ * This configuration is only active when the INSPIRE extension is enabled
  * through the {@code geoserver.extension.inspire.enabled=true} property.
  *
  * @since 2.27.0.0
  */
 @Configuration
-@ConditionalOnClass(name = "org.geoserver.inspire.wcs.WCSExtendedCapabilitiesProvider")
-@ImportFilteredResource({
-    "jar:gs-inspire-.*!/applicationContext.xml#name=^(inspireWcsExtendedCapsProvider|languageCallback|inspireDirManager).*$"
-})
+@TranspileXmlConfig(
+        locations = "jar:gs-inspire-.*!/applicationContext.xml",
+        includes = {"inspireWcsExtendedCapsProvider"})
+@Import({InspireCoreConfiguration.class, InspireConfigurationWcs_Generated.class})
 public class InspireConfigurationWcs {}
