81477260b5c498f5a06f7a41665c05fe2c5f637e
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2025-07-31 00:55:05 -0300

    transpile gs-importer configuration

--- src/geoserver-configuration/src/main/java/org/geoserver/configuration/extension/importer/ImporterWebUIConfiguration.java
+++ src/geoserver-configuration/src/main/java/org/geoserver/configuration/extension/importer/ImporterWebUIConfiguration.java
@@ -1,12 +1,14 @@
 package org.geoserver.configuration.extension.importer;
 
-import org.geoserver.cloud.config.factory.ImportFilteredResource;
+import org.geoserver.spring.config.annotations.TranspileXmlConfig;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * Configuration for the Importer Web UI components.
  */
 @Configuration
-@ImportFilteredResource("jar:gs-importer-web-.*!/applicationContext.xml")
+@TranspileXmlConfig(locations = "jar:gs-importer-web-.*!/applicationContext.xml")
+@Import(ImporterWebUIConfiguration_Generated.class)
 @SuppressWarnings("java:S1118") // Suppress SonarLint warning, constructor needs to be public
 public class ImporterWebUIConfiguration {}
