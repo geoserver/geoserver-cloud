715597b8d9b86f0c704383e65d0dd039d729b57c
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2025-07-31 00:55:05 -0300

    transpile gs-importer configuration

--- src/geoserver-configuration/src/main/java/org/geoserver/configuration/extension/importer/ImporterCoreConfiguration.java
+++ src/geoserver-configuration/src/main/java/org/geoserver/configuration/extension/importer/ImporterCoreConfiguration.java
@@ -4,11 +4,13 @@
  */
 package org.geoserver.configuration.extension.importer;
 
-import org.geoserver.cloud.config.factory.ImportFilteredResource;
+import org.geoserver.spring.config.annotations.TranspileXmlConfig;
+import org.springframework.context.annotation.Import;
 
 /**
  * Configuration for the GeoServer Importer extension.
  */
-@ImportFilteredResource("jar:gs-importer-core-.*!/applicationContext.xml")
+@TranspileXmlConfig(locations = "jar:gs-importer-core-.*!/applicationContext.xml")
+@Import(ImporterCoreConfiguration_Generated.class)
 @SuppressWarnings("java:S1118") // Suppress SonarLint warning, constructor needs to be public
 public class ImporterCoreConfiguration {}
--- src/geoserver-configuration/src/main/java/org/geoserver/configuration/extension/importer/ImporterRestConfiguration.java
+++ src/geoserver-configuration/src/main/java/org/geoserver/configuration/extension/importer/ImporterRestConfiguration.java
@@ -1,12 +1,14 @@
 package org.geoserver.configuration.extension.importer;
 
-import org.geoserver.cloud.config.factory.ImportFilteredResource;
+import org.geoserver.spring.config.annotations.TranspileXmlConfig;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * Configuration for the Importer REST API.
  */
 @Configuration
-@ImportFilteredResource("jar:gs-importer-rest-.*!/applicationContext.xml")
+@TranspileXmlConfig(locations = "jar:gs-importer-rest-.*!/applicationContext.xml")
+@Import(ImporterRestConfiguration_Generated.class)
 @SuppressWarnings("java:S1118") // Suppress SonarLint warning, constructor needs to be public
 public class ImporterRestConfiguration {}
