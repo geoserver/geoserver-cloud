450b6dff2cb5c746e10099379bf7a19463d4fb67
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2025-07-30 23:08:19 -0300

    move gs-graticule configuration to gs-spring-configuration

---
+++ src/geoserver-configuration/src/main/java/org/geoserver/configuration/community/graticule/GraticuleWebUIConfiguration.java
@@ -0,0 +1,22 @@
+/* (c) 2025 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+
+package org.geoserver.configuration.community.graticule;
+
+import org.geoserver.cloud.config.factory.ImportFilteredResource;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration for Graticule extension that provides a data store for
+ * graticule lines.
+ *
+ * <p>
+ * This autoconfiguration class enables the Graticule extension WebUI components
+ *
+ * @since 2.27.0
+ */
+@Configuration
+@ImportFilteredResource("jar:gs-graticule-.*!/applicationContext.xml")
+public class GraticuleWebUIConfiguration {}
