1aa72649fd00527e1c0c1baf85a3db8931b33389
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2025-07-30 23:03:41 -0300

    move gs-geoparquet configuration to gs-spring-configuration

---
+++ src/geoserver-configuration/src/main/java/org/geoserver/configuration/community/geoparquet/GeoParquetWebUIConfiguration.java
@@ -0,0 +1,19 @@
+/* (c) 2025 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+
+package org.geoserver.configuration.community.geoparquet;
+
+import org.geoserver.cloud.config.factory.ImportFilteredResource;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration for GeoParquet extension that provides a data store
+ * configuration panel for the web admin interface.
+ *
+ * @since 2.27.0
+ */
+@Configuration
+@ImportFilteredResource("jar:gs-geoparquet-.*!/applicationContext.xml")
+public class GeoParquetWebUIConfiguration {}
