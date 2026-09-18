52bf83c3bc9d520ec34ada86c480063d5c738c8e
Gabriel Roldan <groldan@multivers.io>
2026-09-18 15:01:30 -0300

    Upgrade cloud-dependencies-bom to 1.1.0
    Upgrades cloud storage SDK dependencies:
    
    - AWS SDK 2.41.32 -> 2.54.16
    - Azure SDK BOM 1.3.3 -> 1.3.8 (azure-storage-blob 12.32.0 -> 12.35.0)
    - google-cloud-storage 2.67.0 -> 2.73.0
    
    See
    - https://github.com/tileverse-io/cloud-dependencies-bom/pull/10

--- src/gwc/blobstores/src/test/java/org/geoserver/cloud/gwc/config/blobstore/AzuriteContainer.java
+++ src/gwc/blobstores/src/test/java/org/geoserver/cloud/gwc/config/blobstore/AzuriteContainer.java
@@ -29,7 +29,7 @@ import org.testcontainers.utility.DockerImageName;
 public class AzuriteContainer extends GenericContainer<AzuriteContainer> {
 
     private static final @NonNull DockerImageName IMAGE_NAME =
-            DockerImageName.parse("mcr.microsoft.com/azure-storage/azurite:3.35.0");
+            DockerImageName.parse("mcr.microsoft.com/azure-storage/azurite:3.37.0");
 
     public final @Getter String accountName = "devstoreaccount1";
     public final @Getter String accountKey =
